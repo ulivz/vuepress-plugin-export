f887cc4de622b977b0f2cf91054e62ee5f306fdd
ULIVZ <472590061@qq.com>
2019-01-28 02:11:58 +0800

    refactor: code style

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer')
 const PDFMerge = require('easy-pdf-merge')
-const { path, fs, logger, chalk } = require('@vuepress/shared-utils')
+const { join } = require('path')
+const { dev } = require('vuepress')
+const { fs, logger, chalk } = require('@vuepress/shared-utils')
+const { red, yellow, gray } = chalk
 
-logger.setOptions({
-  logLevel: 1
-})
+// Keep silent before running custom command.
+logger.setOptions({ logLevel: 1 })
 
 module.exports = (opts, ctx) => ({
   name: 'vuepress-plugin-export',
@@ -14,22 +16,22 @@ module.exports = (opts, ctx) => ({
       .command('export [targetDir]', 'export current vuepress site to a PDF file')
       .allowUnknownOptions()
       .action(async (dir = '.') => {
-        const { dev } = require('vuepress')
-        const { port, host, server } = await dev.prepare(path.resolve(dir), {}, ctx)
+        dir = join(process.cwd(), dir)
+        const { port, host, server } = await dev.prepare(dir, {}, ctx)
+
         server.listen(port, host, async err => {
           if (err) {
-            console.error(chalk.red(err))
+            console.error(red(err))
           }
-          logger.setOptions({
-            logLevel: 3
-          })
+          logger.setOptions({ logLevel: 3 })
           logger.info(`Start to generate current site to PDF ...`)
           try {
             await generatePDF(ctx, port, host)
           } catch (error) {
-            console.error(chalk.red(error))
+            console.error(red(error))
           }
           server.close()
+          // TODO remove process.exit and exit naturaly.
           process.exit(0)
         })
       })
@@ -37,8 +39,8 @@ module.exports = (opts, ctx) => ({
 })
 
 async function generatePDF(ctx, port, host) {
-  const { pages, outDir, tempPath, siteConfig } = ctx
-  const tempDir = path.resolve(tempPath, 'pdf')
+  const { pages, tempPath, siteConfig } = ctx
+  const tempDir = join(tempPath, 'pdf')
   fs.ensureDirSync(tempDir)
 
   const exportPages = pages.map(page => {
@@ -54,20 +56,37 @@ async function generatePDF(ctx, port, host) {
   const browserPage = await browser.newPage()
 
   for (let i = 0; i < exportPages.length; i++) {
-    const { location, path: pagePath, url, title } = exportPages[i]
-    await browserPage.goto(location, { waitUntil: 'networkidle2' })
-    await browserPage.pdf({ path: pagePath, format: 'A4' })
-    logger.success(`Generated ${chalk.yellow(title)} ${chalk.gray(`${url}`)}`)
+    const {
+      location,
+      path: pagePath,
+      url,
+      title
+    } = exportPages[i]
+
+    await browserPage.goto(
+      location,
+      { waitUntil: 'networkidle2' }
+    )
+
+    await browserPage.pdf({
+      path: pagePath,
+      format: 'A4'
+    })
+
+    logger.success(
+      `Generated ${yellow(title)} ${gray(`${url}`)}`
+    )
   }
 
   const files = exportPages.map(({ path }) => path)
   const outputFilename = siteConfig.title || 'site'
-  await new Promise((resolve, reject) => {
-    PDFMerge(files, `${outputFilename}.pdf`, (err) => {
+  const outputFile = `${outputFilename}.pdf`
+  await new Promise(resolve => {
+    PDFMerge(files, outputFile, err => {
       if (err) {
-        reject(err)
+        throw err
       }
-      logger.success(`Export ${chalk.yellow(`${outputFilename}.pdf`)} file!`)
+      logger.success(`Export ${yellow(outputFile)} file!`)
       resolve()
     })
   })
