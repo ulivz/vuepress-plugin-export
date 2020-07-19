60a84bc400f090eadaa465800abb30405e2088ad
Patrick Sullivan <sullivanpt@acm.org>
2020-07-19 10:41:06 -0700

    feat: adds multi-export options.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { red, yellow, gray } = chalk
 // Keep silent before running custom command.
 logger.setOptions({ logLevel: 1 })
 
-module.exports = (opts = {}, ctx) => ({
+module.exports = (options = {}, context) => ({
   name: 'vuepress-plugin-export',
 
   chainWebpack(config) {
@@ -24,23 +24,27 @@ module.exports = (opts = {}, ctx) => ({
       .action(async (dir = '.') => {
         dir = join(process.cwd(), dir)
         try {
-          const nCtx = await dev({
+          const devContext = await dev({
             sourceDir: dir,
             clearScreen: false,
-            theme: opts.theme || '@vuepress/default'
+            theme: options.theme || '@vuepress/default'
           })
+
           logger.setOptions({ logLevel: 3 })
           logger.info(`Start to generate current site to PDF ...`)
+
           try {
-            await generatePDF(nCtx, {
-              port: nCtx.devProcess.port,
-              host: nCtx.devProcess.host,
-              sorter: opts.sorter
+            await generatePDF(devContext, {
+              port: devContext.devProcess.port,
+              host: devContext.devProcess.host,
+              base: devContext.base,
+              options
             })
           } catch (error) {
             console.error(red(error))
           }
-          nCtx.devProcess.server.close()
+
+          devContext.devProcess.server.close()
           process.exit(0)
         } catch (e) {
           throw e
@@ -49,45 +53,79 @@ module.exports = (opts = {}, ctx) => ({
   }
 })
 
-async function generatePDF(ctx, {
-  port,
-  host,
-  sorter,
-}) {
-  const { pages, tempPath, siteConfig } = ctx
-  const tempDir = join(tempPath, 'pdf')
-  fs.ensureDirSync(tempDir)
-
-  let exportPages = pages.slice(0)
-
-  if (typeof sorter === 'function') {
-    exportPages = exportPages.sort(sorter)
+const filter = (filter) => {
+  if (typeof filter === 'function') {
+    return filter(page)
   }
 
-  exportPages = exportPages.map(page => {
-    return {
-      url: page.path,
-      title: page.title,
-      location: `http://${host}:${port}${page.path}`,
-      path: `${tempDir}/${page.key}.pdf`
-    }
-  })
+  return (page) => {
+    return !filter || filter.test(page.location)
+  }
+}
+
+const createOutputFilename = (dest, siteConfig, fallback) => {
+  if (typeof dest === 'function') {
+    return dest(siteConfig)
+  }
+
+  if (typeof dest === 'string') {
+    return dest
+  }
+
+  return `${siteConfig.title || String(fallback)}.pdf`
+}
+
+async function generatePDF(context, {
+  port,
+  host,
+  base,
+  // options can be either a single or multiple option objects
+  options: multiOptions,
+}) {
+  const { pages, tempPath, siteConfig } = context
+  const tempDir = join(tempPath, 'pdf')
+
+  console.log('+++ base', base);
+
+  multiOptions = Array.isArray(multiOptions) ? multiOptions : [multiOptions]
+
+  fs.ensureDirSync(tempDir)
 
   const browser = await puppeteer.launch()
   const browserPage = await browser.newPage()
 
-  for (let i = 0; i < exportPages.length; i++) {
+  // Generate all pages on bulk
+  const exportPages = pages.slice(0).map(page => {
+    return {
+      url: page.path,
+      title: page.title,
+      location: `http://${host}:${port}${base || ''}${page.path}`.replace(/\/\//g, '/'),
+      path: `${tempDir}/${page.key}.pdf`
+    }
+  })
+
+  console.log('+++ export pages', exportPages);
+
+  const promises = exportPages.map(async exportPage => {
     const {
       location,
       path: pagePath,
       url,
       title
-    } = exportPages[i]
+    } = exportPage
+
+    console.log('+++ goto', location);
 
     await browserPage.goto(
       location,
       { waitUntil: 'networkidle2' }
     )
+
+    console.log('+++ went', location);
+
+    await new Promise(resolve => setTimeout(resolve, 5000))
+
+    console.log('+++ wait');
 
     await browserPage.pdf({
       path: pagePath,
@@ -95,24 +133,38 @@ async function generatePDF(ctx, {
     })
 
     logger.success(`Generated ${yellow(title)} ${gray(`${url}`)}`)
-  }
 
-  const files = exportPages.map(({ path }) => path)
-  const outputFilename = siteConfig.title || 'site'
-  const outputFile = `${outputFilename}.pdf`
-
-  await new Promise(resolve => {
-    PDFMerge(files, outputFile, err => {
-      if (err) {
-        throw err
-      }
-      logger.success(`Export ${yellow(outputFile)} file!`)
-      resolve()
-    })
+    console.log('+++ success', title, url);
   })
 
+  await Promise.all(promises);
   await browser.close()
-  fs.removeSync(tempDir)
+
+  for (const options of multiOptions) {
+    const files = exportPages
+      .filter(filter(options.filter))
+      .sort(options.sorter)
+      .map(({ path }) => path)
+
+    const outputFile = createOutputFilename(options.dest, siteConfig, 'site')
+
+    if (files.length === 1) {
+      const [filename] = files;
+      console.log('+++ filename', filename, outputFile)
+      fs.copyFileSync(filename, outputFile)
+    } else {
+      console.log('+++ merge', files);
+      await new Promise(resolve => {
+        PDFMerge(files, outputFile, err => {
+          if (err) {
+            throw err
+          }
+          logger.success(`Export ${yellow(outputFile)} file!`)
+          resolve()
+        })
+      })
+    }
+  }
+
+  // fs.removeSync(tempDir)
 }
-
-
